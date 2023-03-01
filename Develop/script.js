8279d31880fccf981d1c2b60ae9a9e197c66df02
castarke <candler.starke@yahoo.com>
2023-02-28 20:31:26 -0500

    made minor changes to the HTML and scriptfile. added in comments to the html and script and changed the name of the screenshot. completed most of the readme

--- Develop/script.js
+++ Develop/script.js
@@ -1,30 +1,33 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+
 var saveButton = document.querySelector(".saveBtn")
 var date = dayjs().format('dddd, MMMM D YYYY hh:mma');
 $("#currentTime").html(date);
+
 
 $(document).ready(function() {
 })
 
 
-
+// changing color of cell blocks based on the time
 function colorChange() {
   var time = dayjs().get('hour');
   $(".time-block").each(function() {
   var eventTime = parseInt($(this).attr("id").split("hour")[1]);
-
+// changing color to red for present time
   if (eventTime === time) {
     $(this).removeClass("past");
     $(this).removeClass("future");
     $(this).addClass("present") ;
-  }
+  // changing color to gray for past
   else if (eventTime < time) {
     $(this).removeClass("future");
     $(this).removeClass("present");
     $(this).addClass("past")
   }
+  // changing color to green for future
   else {
       $(this).removeClass("present");
       $(this).removeClass("past");
@@ -33,6 +36,7 @@ function colorChange() {
 })
 };
 
+// setting up the save button so it will save the plans and the hours to local storage
 $(".saveBtn").on('click',function(){
   var plans = $(this).siblings(".description").val();
   var hour = $(this).parent().attr("id");
@@ -40,6 +44,7 @@ $(".saveBtn").on('click',function(){
   localStorage.setItem (hour, plans);
  })
 
+//  setting up a clear button to clear local storage and to clear the plans from their blocks
  $('.clearBtn').on('click', function(event){
   localStorage.clear();
   $('textarea').val("");
@@ -48,37 +53,25 @@ $(".saveBtn").on('click',function(){
 
 colorChange();
 
+// 8am from local storage
 $("#hour8 .description").val(localStorage.getItem("hour8"));
+// 9am from local storage
 $("#hour9 .description").val(localStorage.getItem("hour9"));
+// 10 am from local storage
 $("#hour10 .description").val(localStorage.getItem("hour10"));
+// 11am from local storage
 $("#hour11 .description").val(localStorage.getItem("hour11"));
+// 12pm from local storage
 $("#hour12 .description").val(localStorage.getItem("hour12"));
+// 1pm from local storage
 $("#hour13 .description").val(localStorage.getItem("hour13"));
+// 2pm from local storage
 $("#hour14 .description").val(localStorage.getItem("hour14"));
+// 3pm from local storage
 $("#hour15 .description").val(localStorage.getItem("hour15"));
+// 4pm from local storage
 $("#hour16 .description").val(localStorage.getItem("hour16"));
+// 5pm from local storage
 $("#hour17 .description").val(localStorage.getItem("hour17"));
+// 6pm from local storage
 $("#hour18 .description").val(localStorage.getItem("hour18"));
-  
-
-// $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
-// });
-
--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # daily-planner
+
+## Description
+
+I have created a daily planner to help keep track of plans and tasks through out the day from 8am - 6pm. For times that are in the past, the blocks will be gray. For current time, the block will be red. For all future tasks, the blocks will be green. You can also find the current date and time at the top of the screen to help you keep track of your day. Mid page you will find a "Clear ALL the STUFF" button that will clear the local storage as well as the text within the blocks. 
+
+## Table of Contents 
+
+If your README is long, add a table of contents to make it easy for users to find what they need.
+
+    -Assets
+        -daily.planner.screenshot.png
+    -Develop
+        -script.js
+        -style.js
+    -index.html
+    -README.md
+
+## Usage
+
+URL to daily planner: https://castarke.github.io/daily-planner/
+
+Screenshot:
